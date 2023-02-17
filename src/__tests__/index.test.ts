d9cfb49e2269e0ecbc8c8e9ce539cda2c56bfcf0
Kevin McGill <kevin.mcgill@caseys.com>
2023-02-17 15:29:59 -0600

    WIP

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,47 +1,54 @@
-import { ApolloServer, ApolloServerOptions, BaseContext } from '@apollo/server';
-import { startServerAndCreateHandler } from '..';
+import {ApolloServer, ApolloServerOptions, BaseContext} from '@apollo/server';
+import {startServerAndCreateHandler} from '..';
 import {
-  CreateServerForIntegrationTestsOptions,
-  defineIntegrationTestSuite,
+    CreateServerForIntegrationTestsOptions,
+    defineIntegrationTestSuite,
 } from '@apollo/server-integration-testsuite';
-import { createServer } from 'http';
-import type { AzureFunction } from '@azure/functions';
-import { createMockServer, urlForHttpServer } from './mockServer';
+import {createServer} from 'http';
+import type {AzureFunction} from '@azure/functions';
+import {createMockServer, urlForHttpServer} from './mockServer';
 
 describe('Azure Functions', () => {
-  defineIntegrationTestSuite(
-    async function (
-      serverOptions: ApolloServerOptions<BaseContext>,
-      testOptions?: CreateServerForIntegrationTestsOptions,
-    ) {
-      const httpServer = createServer();
-      const server = new ApolloServer({
-        ...serverOptions,
-      });
+    defineIntegrationTestSuite(
+        async function (
+            serverOptions: ApolloServerOptions<BaseContext>,
+            testOptions?: CreateServerForIntegrationTestsOptions,
+        ) {
+            const httpServer = createServer();
+            const server = new ApolloServer({
+                ...serverOptions,
+            });
 
-      const handler: AzureFunction = testOptions
-        ? startServerAndCreateHandler(server, testOptions)
-        : startServerAndCreateHandler(server);
+            const handler: AzureFunction = testOptions
+                ? startServerAndCreateHandler(server, testOptions)
+                : startServerAndCreateHandler(server);
 
-      await new Promise<void>((resolve) => {
-        httpServer.listen({ port: 0 }, resolve);
-      });
+            await new Promise<void>((resolve) => {
+                httpServer.listen({port: 0}, resolve);
+            });
 
-      httpServer.addListener('request', createMockServer(handler));
+            httpServer.addListener('request', createMockServer(handler));
 
-      return {
-        server,
-        url: urlForHttpServer(httpServer),
-        async extraCleanup() {
-          await new Promise<void>((resolve) => {
-            httpServer.close(() => resolve());
-          });
+            return {
+                server,
+                url: urlForHttpServer(httpServer),
+                async extraCleanup() {
+                    await new Promise<void>((resolve) => {
+                        httpServer.close(() => resolve());
+                    });
+                },
+            };
         },
-      };
-    },
-    {
-      serverIsStartedInBackground: true,
-      noIncrementalDelivery: true,
-    },
-  );
+        {
+            serverIsStartedInBackground: true,
+            noIncrementalDelivery: true,
+        },
+    );
+
+    describe('parseBody', () => {
+
+        test('can parse application/json', () => {});
+        test('can parse application/json; charset=utf-8', () => {});
+        test('can parse application/graphql-response+json;charset=utf-8', () => {});
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,109 @@
 import type {
-  AzureFunction,
-  Context,
-  HttpRequest,
-  HttpRequestHeaders,
+    AzureFunction,
+    Context,
+    HttpRequest,
+    HttpRequestHeaders,
 } from '@azure/functions';
 import {
-  ApolloServer,
-  BaseContext,
-  ContextFunction,
-  HeaderMap,
-  HTTPGraphQLRequest,
+    ApolloServer,
+    BaseContext,
+    ContextFunction,
+    HeaderMap,
+    HTTPGraphQLRequest,
 } from '@apollo/server';
-import type { WithRequired } from '@apollo/utils.withrequired';
+import type {WithRequired} from '@apollo/utils.withrequired';
 
 export interface AzureFunctionsContextFunctionArgument {
-  context: Context;
+    context: Context;
 }
 
 export interface AzureFunctionsMiddlewareOptions<TContext extends BaseContext> {
-  context?: ContextFunction<[AzureFunctionsContextFunctionArgument], TContext>;
+    context?: ContextFunction<[AzureFunctionsContextFunctionArgument], TContext>;
 }
 
 const defaultContext: ContextFunction<
-  [AzureFunctionsContextFunctionArgument],
-  any
+    [AzureFunctionsContextFunctionArgument],
+    any
 > = async () => ({});
 
 export function startServerAndCreateHandler(
-  server: ApolloServer<BaseContext>,
-  options?: AzureFunctionsMiddlewareOptions<BaseContext>,
+    server: ApolloServer<BaseContext>,
+    options?: AzureFunctionsMiddlewareOptions<BaseContext>,
 ): AzureFunction;
 export function startServerAndCreateHandler<TContext extends BaseContext>(
-  server: ApolloServer<TContext>,
-  options: WithRequired<AzureFunctionsMiddlewareOptions<TContext>, 'context'>,
+    server: ApolloServer<TContext>,
+    options: WithRequired<AzureFunctionsMiddlewareOptions<TContext>, 'context'>,
 ): AzureFunction;
 export function startServerAndCreateHandler<TContext extends BaseContext>(
-  server: ApolloServer<TContext>,
-  options?: AzureFunctionsMiddlewareOptions<TContext>,
+    server: ApolloServer<TContext>,
+    options?: AzureFunctionsMiddlewareOptions<TContext>,
 ): AzureFunction {
-  server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
-  return async (context: Context, req: HttpRequest) => {
-    const contextFunction = options?.context ?? defaultContext;
-    try {
-      const normalizedRequest = normalizeRequest(req);
+    server.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
+    return async (context: Context, req: HttpRequest) => {
+        const contextFunction = options?.context ?? defaultContext;
+        try {
+            const normalizedRequest = normalizeRequest(req);
 
-      const { body, headers, status } = await server.executeHTTPGraphQLRequest({
-        httpGraphQLRequest: normalizedRequest,
-        context: () => contextFunction({ context }),
-      });
+            const {body, headers, status} = await server.executeHTTPGraphQLRequest({
+                httpGraphQLRequest: normalizedRequest,
+                context: () => contextFunction({context}),
+            });
 
-      if (body.kind === 'chunked') {
-        throw Error('Incremental delivery not implemented');
-      }
+            if (body.kind === 'chunked') {
+                throw Error('Incremental delivery not implemented');
+            }
 
-      return {
-        status: status || 200,
-        headers: {
-          ...Object.fromEntries(headers),
-          'content-length': Buffer.byteLength(body.string).toString(),
-        },
-        body: body.string,
-      };
-    } catch (e) {
-      context.log.error('Failure processing GraphQL request', e);
-      return {
-        status: 400,
-        body: (e as Error).message,
-      };
-    }
-  };
+            return {
+                status: status || 200,
+                headers: {
+                    ...Object.fromEntries(headers),
+                    'content-length': Buffer.byteLength(body.string).toString(),
+                },
+                body: body.string,
+            };
+        } catch (e) {
+            context.log.error('Failure processing GraphQL request', e);
+            return {
+                status: 400,
+                body: (e as Error).message,
+            };
+        }
+    };
 }
 
 function normalizeRequest(req: HttpRequest): HTTPGraphQLRequest {
-  if (!req.method) {
-    throw new Error('No method');
-  }
+    if (!req.method) {
+        throw new Error('No method');
+    }
 
-  return {
-    method: req.method,
-    headers: normalizeHeaders(req.headers),
-    search: new URL(req.url).search,
-    body: parseBody(req.body, req.headers['content-type']),
-  };
+    return {
+        method: req.method,
+        headers: normalizeHeaders(req.headers),
+        search: new URL(req.url).search,
+        body: parseBody(req.method, req.body, req.headers['content-type']),
+    };
 }
 
 function parseBody(
-  body: string | null | undefined,
-  contentType: string | undefined,
-): object | string {
-  if (body) {
-    if (contentType === 'application/json') {
-      if (typeof body === 'string') {
+    method: string | undefined,
+    body: string | null | undefined,
+    contentType: string | undefined,
+): object | null {
+    const isValidContentType = contentType?.startsWith('application/json') &&
+        contentType?.startsWith('application/graphql-response+json');
+    const isValidPostRequest = method === 'POST' &&
+        typeof body === 'string' && isValidContentType;
+
+    if (isValidPostRequest) {
         return JSON.parse(body);
-      }
-      return body;
     }
-  }
-  return '';
+    return null;
 }
 
 function normalizeHeaders(headers: HttpRequestHeaders): HeaderMap {
-  const headerMap = new HeaderMap();
-  for (const [key, value] of Object.entries(headers)) {
-    headerMap.set(key, value ?? '');
-  }
-  return headerMap;
+    const headerMap = new HeaderMap();
+    for (const [key, value] of Object.entries(headers)) {
+        headerMap.set(key, value ?? '');
+    }
+    return headerMap;
 }
